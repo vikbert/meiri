ed663ee8351c759411ca2fffad9275d78a0d2326
Xun Zhou <xun.zhou@lidl.com>
2019-05-22 08:54:01 +0200

    deploy: github gh-pages 2019-05-22 08:54:01

---
+++ precache-manifest.66f7a219ec4832a29ff4391bc1eabc20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda21b12abaadc953cbb",
+    "url": "/meiri/static/js/runtime~main.5d229a3d.js"
+  },
+  {
+    "revision": "0a5268dfd4e22f77770a",
+    "url": "/meiri/static/js/main.cb93bcc2.chunk.js"
+  },
+  {
+    "revision": "e402691cae4397fa8c56",
+    "url": "/meiri/static/js/2.2f8cebb7.chunk.js"
+  },
+  {
+    "revision": "0a5268dfd4e22f77770a",
+    "url": "/meiri/static/css/main.7522adf9.chunk.css"
+  },
+  {
+    "revision": "0fd44577c0bb22ab91f8ae0327eaa437",
+    "url": "/meiri/index.html"
+  }
+];
